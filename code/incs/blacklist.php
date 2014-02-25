513fc8c285090261669f95ac2e5d54a78fc240f0
Cydrobolt <summermontreal@gmail.com>
2014-02-25 18:11:48 -0500

    Update blacklist.php

--- code/incs/blacklist.php
+++ code/incs/blacklist.php
@@ -1,16 +1,11 @@
 <?php
-//Review consider changing to fwrite & fread instead database
-//Errors,errors
-//
-//
+//Perhaps array?
 $IP = $_SERVER['REMOTE_ADDR'];
 include("mysqlcxn.php");
-$query = "SELECT ip FROM blacklist";
+$query = "SELECT ip FROM blacklist WHERE ip='{$IP}'";
 $result = mysqli_query($cxn,$query);
 $row = mysqli_fetch_assoc($result);
-if($row != ""){
-foreach($row as $val){
-    if($val==$IP){
+if($row){
         header("location:/Ptrack/denied.php");
         mysqli_free_result($result);
     }
@@ -18,9 +13,6 @@ foreach($row as $val){
         echo'<!--accepted-->';
         mysqli_free_result($result);
     }
-    
-}
-}
 else {
     echo'<!--blacklist empty-->';
 }
